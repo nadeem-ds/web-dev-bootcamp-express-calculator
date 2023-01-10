b4b8705b106f32805b4c90200c974e1befb702bd
Nadeem <anwarnadeem2010@gmail.com>
2023-01-10 23:52:57 +0530

    BMI Calculator challenge

--- calculator.js
+++ calculator.js
@@ -9,12 +9,23 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function (req, res) {
   console.log(req.body);
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("Thanks for posting " + result);
+});
+
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("Your Bmi " + bmi);
 });
 
 app.listen("3000", function () {
